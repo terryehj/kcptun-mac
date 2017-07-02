c9be86f440f3d5763560191bb6e306851928e0cb
Terry Yi <terry.ehj@gmail.com>
2017-07-02 22:14:56 +0800

    1

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
-ps -ef | grep client_darwin_amd64 | grep -v grep | awk '{print $2}' | xargs sudo kill -9
-sudo ./client_darwin_amd64 -c config.json &> kcptun.log &
+ps -ef | grep client_darwin_amd64 | grep -v grep | awk '{print $2}' | xargs kill -9
+./client_darwin_amd64 -c config.json &> kcptun.log &
--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -ef | grep client_darwin_amd64 | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+ps -ef | grep client_darwin_amd64 | grep -v grep | awk '{print $2}' | xargs kill -9
